6a145d176c09d0b7bc84a1ef0ef202a3c1d48162
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-07-16 06:01:34 +0330

    add room joining functionality + add private message functionality + add admin-ui for socket monitoring

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,10 +1,35 @@
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 var io = new Server({
     cors: {
-        origin: ["http://localhost:8080", "http://127.0.0.1:5500"],
+        origin: ["http://127.0.0.1:5500", "https://admin.socket.io"],
+        credentials: true,
     },
 });
 io.on("connection", function (socket) {
-    console.log(socket.id);
+    socket.on("send-message", function (_a) {
+        var message = _a.message, room = _a.room;
+        if (!room) {
+            socket.broadcast.emit("recieve-message", message);
+        }
+        else {
+            socket.to(room).emit("recieve-message", message);
+        }
+    });
+    socket.on("join-room", function (room) {
+        try {
+            socket.join(room);
+            socket
+                .to(room)
+                .emit("recieve-message", "".concat(socket.id, " joined ").concat(room, " room"));
+            socket.emit("chat-header", room);
+            socket.emit("recieve-message", "you joined ".concat(room, " room"));
+        }
+        catch (e) {
+            console.log("[error]", "join room :", e);
+            socket.emit("recieve-message", "couldnt perform requested action");
+        }
+    });
 });
+instrument(io, { auth: false, mode: "development" });
 io.listen(3000);
--- server/dist/server.ts
+++ server/dist/server.ts
@@ -1,13 +1,38 @@
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
 const io = new Server({
   cors: {
-    origin: ["http://localhost:8080", "http://127.0.0.1:5500"],
+    origin: ["http://127.0.0.1:5500", "https://admin.socket.io"],
+    credentials: true,
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  socket.on(
+    "send-message",
+    ({ message, room }: { message: string; room?: string }) => {
+      if (!room) {
+        socket.broadcast.emit("recieve-message", message);
+      } else {
+        socket.to(room).emit("recieve-message", message);
+      }
+    }
+  );
+  socket.on("join-room", function (room) {
+    try {
+      socket.join(room);
+      socket
+        .to(room)
+        .emit("recieve-message", `${socket.id} joined ${room} room`);
+      socket.emit("chat-header", room);
+      socket.emit("recieve-message", `you joined ${room} room`);
+    } catch (e) {
+      console.log("[error]", "join room :", e);
+      socket.emit("recieve-message", "couldnt perform requested action");
+    }
+  });
 });
 
+instrument(io, { auth: false, mode: "development" });
 io.listen(3000);
